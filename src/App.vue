90a882185eb1c7203d2f3cc85824cba4b038e4f0
LaupWing <loc_strict@hotmail.com>
2021-06-30 21:27:13 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-   <img alt="Vue logo" src="./assets/logo.png" />
+   <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
 </template>
 
 <script>
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+
 export default {
    name: "App",
+   data() {
+      return {
+         editor: ClassicEditor,
+         editorData: "<p>Content of the editor.</p>",
+         editorConfig: {
+            // The configuration of the editor.
+         },
+      };
+   },
 };
 </script>
 
