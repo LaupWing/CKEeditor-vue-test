2381d2ce5ea4fe794e319fb6c85dd262921c0a1a
LaupWing <loc_strict@hotmail.com>
2021-07-14 08:51:11 +0200

    heh

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,17 @@
 
 <script>
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
-import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document/src/ckeditor';
+import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
 
 export default {
    name: "App",
    data() {
       return {
-         editor: ClassicEditor,
+         editor: DecoupledEditor,
          editorData: "<p>Content of the editor.</p>",
          editorConfig: {
             plugins:[
-               DecoupledEditor
+               
             ]
             // The configuration of the editor.
          },
