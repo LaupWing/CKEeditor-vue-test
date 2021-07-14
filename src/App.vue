4edb587c0d25a3ea1cc9c0ea42e9f7e2ded52b6b
LaupWing <loc_strict@hotmail.com>
2021-07-14 09:03:07 +0200

    heh

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,38 @@
 <template>
-   <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
+   <ckeditor
+      :editor="editor"
+      v-model="editorData"
+      :config="editorConfig"
+   ></ckeditor>
 </template>
 
 <script>
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
-import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+// import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
+import Pagination from "@ckeditor/ckeditor5-pagination/src/pagination";
 
 export default {
    name: "App",
    data() {
       return {
-         editor: DecoupledEditor,
+         editor: ClassicEditor,
          editorData: "<p>Content of the editor.</p>",
          editorConfig: {
             plugins:[
-               
-            ]
+               Pagination
+            ],
+            pagination: {
+               // A4
+               pageWidth: "21cm",
+               pageHeight: "29.7cm",
+   
+               pageMargins: {
+                  top: "20mm",
+                  bottom: "20mm",
+                  right: "12mm",
+                  left: "12mm",
+               },
+            },
             // The configuration of the editor.
          },
       };
